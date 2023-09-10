552cd620fe062b01fe7350bf36ff4c27676b0196
Anurag Deep <anuragdeep.xon@gmail.com>
2023-09-10 11:37:56 +0530

    FEAT: UI/UX update

--- src/popup.js
+++ src/popup.js
@@ -1,6 +1,8 @@
 import $ from 'jquery';
 import './styles.css';
 
-$(document).ready(function() {
-  $('#message').text('Hello from jQuery and Tailwind!');
+$(document).ready(function () {
+  $('#sayHiButton').click(function () {
+    alert('Hello');
+  });
 });
